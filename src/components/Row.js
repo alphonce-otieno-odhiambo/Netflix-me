0eead810c1a0ed8c5885db94be27139773b43975
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-06 20:36:04 +0300

    fetch the request

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,13 +1,31 @@
 import React, {useState, useEffect} from 'react'
+import axios from  'axios';
 
-function Row({title}) {
+
+function Row({title, fetchUlr}) {
 
     const [movies , setMovies] = useState([]);
 
-    /** specific running using seEffect*/ 
+    /** specific running using useEffect*/ 
+    useEffect = (() => {
+
+        async function fetchData(){
+            const request = await axios.get(fetchUlr);
+            setMovies(request.data.results);
+            
+            return request;
+        }
+        fetchData()
+    }, [fetchUlr]);
+    
   return (
-    <div>
+    <div className='row'>
         <h2>{title} </h2>
+        <div className='row-poster'>
+            {movies.map(movie => {
+                <img src={movie.poster_path} alt={movie.name}/>
+            })}
+        </div>
     </div>
   )
 }
