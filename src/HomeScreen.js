ee3b58d8faca63d8c608b1322e9ec610acd4371c
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-08 02:16:49 +0300

    fetch the movie data

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -10,13 +10,12 @@ function HomeScreen() {
     <div className='homeScreen'>
         <Nav/>
         <Baner/>
-        <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-        <Row title='Trending Movies' fetchUrl={requests.fetchTrending}/>
-        <Row title='Top Rated movies' fetchUrl={requests.fetchTopRated}/>
-        <Row title='Commedy Movies' fetchUrl={requests.fetchComedyMovies}/>
-        <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies}/>
-        <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies}/>
-        <Row title='Documentary Movies' fetchUrl={requests.fetchDocumentaries}/>
+        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/> 
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
