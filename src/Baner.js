9bd836eda80e54617306a6b32e4db5c7b7a5e9a9
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-07 18:11:01 +0300

    cfetch the backdrop_path img

--- src/Baner.js
+++ src/Baner.js
@@ -1,9 +1,28 @@
-import React, {useState, useEffect} from 'react'
-import './Baner.css'
+
+import React, {useState, useEffect} from 'react';
+import './Baner.css';
+import axios from './axios';
+import requests from './Request';
+
 
 function Baner() {
 
-    const [movie, setMovie] = useState([])
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results [
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData()
+
+    }, [])
+    console.log(movie)
 
     function trancate(string, n){
         return string?.length > n? string.substr(0, n-1) + '...' : string;
@@ -11,7 +30,7 @@ function Baner() {
   return (
     <header className='baner' style={{
         backgroundSize: 'cover',
-        backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEvXZ2CPQffI2fT6hAsjO4c8KswvsD80KmA&usqp=CAU')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
         backgroundPosition: 'center',
     } }>
         <div className='baner_contents'>
