88d5fb5e5daa62ed43aed84e27c518914697cd43
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-07 15:18:29 +0300

    create a netflix banner

--- src/Baner.js
+++ src/Baner.js
@@ -3,9 +3,24 @@ import './Baner.css'
 
 function Baner() {
   return (
-    <div className='baner'>
+    <header className='baner' style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEvXZ2CPQffI2fT6hAsjO4c8KswvsD80KmA&usqp=CAU')`,
+        backgroundPosition: 'center',
+    } }>
+        <div className='baner_contents'>
+            <h1 className='baner_title'>MOVIE NAME</h1>
+            <div className='baner_buttons'>
+                <button className='baner_btn'>Play</button>
+                <button className='baner_btn'>My List</button>
+            </div>
+            <div className='baner_discrp'>
+                This is a test description for the banner 
+            </div>
+        </div>
+    </header> 
         
-    </div>
+   
   )
 }
 
